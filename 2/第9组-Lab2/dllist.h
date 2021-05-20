87e3561c9469275ecc6113207c0916e8a9c2ba89
GoatWu <goatwu@qq.com>
2021-05-20 10:40:18 +0800

    完成实验2的报告和提交材料

---
+++ 2/第9组-Lab2/dllist.h
@@ -0,0 +1,59 @@
+#include "utility.h"
+#include "system.h"
+#include "synch.h"
+#define INITKEY 100
+
+class DLLElement { 
+public:   
+    DLLElement( void *itemPtr, int sortKey );     // initialize a list element  
+    DLLElement *next;           // next element on list
+                                // NULL if this is the last  
+    DLLElement *prev;           // previous element on list// NULL if this is the first  
+    int key;                    // priority, for a sorted list  
+    void *item;                 // pointer to item on the list
+}; 
+
+class DLList { 
+public:   
+    DLList(int errType = 0);        // initialize the list          
+    ~DLList();                  // de-allocate the list  
+    void Prepend(void *item);   // add to head of list (set key = min_key-1)  
+    void Append(void *item);    // add to tail of list (set key = max_key+1)  
+    void *Remove(int *keyPtr);  // remove from head of list
+                                // set *keyPtr to key of the removed item
+                                // return item (or NULL if list is empty)  
+    bool IsEmpty();             // return true if list has elements  
+
+    // routines to put/get items on/off list in order (sorted by key)  
+    void SortedInsert(void *item, int sortKey);   
+    void *SortedRemove(int sortKey);  // remove first item with key==sortKey
+                                      // return NULL if no such item exists
+
+    void printList();
+
+private:   
+    DLLElement *first;          // head of the list, NULL if empty  
+    DLLElement *last;           // last element of the list, NULL if empty
+
+    int errType;                // buggy behaviors to be demonstrate
+                                // range from 0 to 4
+};
+
+class SynchDLList {
+public:
+    SynchDLList(int errType = 0);   // 构造函数
+    ~SynchDLList();  // 析构函数
+    void *Remove(int *keyPtr);  // remove from head of list
+                                // set *keyPtr to key of the removed item
+                                // return item (or NULL if list is empty)
+    void SortedInsert(void *item, int sortKey);
+    void printList();
+
+private:
+    DLList *list;
+    Lock *lock;
+    Condition *empty;
+};
+
+void InsertList(int threadNum, int N, SynchDLList *slist);
+void RemoveList(int threadNum, int N, SynchDLList *slist);
